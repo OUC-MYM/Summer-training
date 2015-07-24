501db0ba8ec3c54f0c43860014c4ee29d94809a7
OUC-MYM <570109323@qq.com>
2015-07-24 23:10:06 +0800

    AC

--- 7-24/Round and Round We Go.cpp
+++ 7-24/Round and Round We Go.cpp
@@ -1,65 +1,70 @@
 #include <iostream>
+#include <cstdio>
 #include <cstring>
 using namespace std;
 
-char str[80];
-char temp[80];
+char num[80];
 int len;
+char toch[80];
 int ok;
 
-void cyclic(char ch[])
+bool bigmul(int i)
 {
-    ch[79]=ch[0];
-    for(int i=0; i<len-1; i++)
-        ch[i]=ch[i+1];
-    ch[len-1]=ch[79];
+    int yu=0;
+    for(int j=len-1; j>=0; j--)
+    {
+        int temp=i*(num[j]-'0')+yu;
+        yu=temp/10;
+        toch[j]=temp%10+'0';
+    }
+    cout << toch << endl;
+    if(yu/10>0)
+    {
+        return false;
+    }
+    return true;
 }
 
-void check(char ch[])
+bool ifcircle()
 {
-    char n[80];
-
-    for(int k=2; k<=n[0]-'0'; k++)
-    {
-        if(!ok)
-            break;
-        strcpy(n,ch);
-
-        int yu=n[0]-'0'-k;
-
-        for(int i=1; i<len; i++)
+    int ok=0;
+    for(int j=0; j<len; j++)
+        if(toch[j]==num[0])
         {
-            int temp=n[i]+yu*10;
-            yu=temp%k;
+            ok=1;
+            for(int t=j; t<j+len; t++)
+                    if(toch[t%len]!=num[t-j])
+                        ok=0;
+            if(ok)
+                return true;
         }
-        if(yu!=0)
-            ok=0;
-    }
+    if(!ok)
+        return false;
+    return true;
 }
 
 int main()
 {
-    while(cin >> str)
+    while (cin>>num)
     {
-        len=strlen(str);
         ok=1;
-
-        strcpy(temp,str);
-
-        for(int i=0; i<len; i++)
+        len=strlen(num);
+        for(int i=2; i<=len; i++)
         {
-            if(!ok)
+            if(!bigmul(i))
+            {
+                ok=0;
                 break;
-            cyclic(str);
-            cout << str << endl;
-            check(str);
+            }
+            if(!ifcircle())
+            {
+                ok=0;
+                break;
+            }
         }
-
-        if(!ok)
-            cout << temp << " is not cyclic" << endl;
+        if(ok)
+            printf("%s is cyclic\n",num);
         else
-            cout << temp << " is cyclic" << endl;
-
+            printf("%s is not cyclic\n",num);
     }
-    return 0;
 }
